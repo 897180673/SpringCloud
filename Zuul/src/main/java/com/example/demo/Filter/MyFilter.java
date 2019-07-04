8738bc8bf36a2dac3874d5e43da31d8cacbc7d0e
saltedFish <>
2019-07-04 23:33:46 +0800

    添加网关

---
+++ Zuul/src/main/java/com/example/demo/Filter/MyFilter.java
@@ -0,0 +1,40 @@
+package com.example.demo.Filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String requestUri=request.getRequestURI();
+        log.info("###请求Uri为:"+requestUri);
+
+
+        return null;
+    }
+}
