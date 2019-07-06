4d46c6fc4914ce561daf82bd5d78ec45598e3c9c
saltedFish <>
2019-07-06 19:25:19 +0800

    ServiceA添加 断路器监控

--- ServiceA/src/main/java/com/example/demo/controller/TestController.java
+++ ServiceA/src/main/java/com/example/demo/controller/TestController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import brave.sampler.Sampler;
 import com.example.demo.feign.MyFeignClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +40,14 @@ public class TestController {
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
     }
+
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
 }
